d5a902fbbc4b2d18e54c4e35cdbb80d95bf0cfd8
董 纹陶 [SVR III] <dongwentao@lifang.com>
2017-04-26 17:23:56 +0800

    no message

--- src/test/java/com/dongwt/java/problems/proxy/java/JavaProxyTest.java
+++ src/test/java/com/dongwt/java/problems/proxy/java/JavaProxyTest.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Proxy;
 
 
 /**
-* @Description: java动态代理
+* @Description: java动态代理(基于接口代理)
 * @Author: dongwt
 * @Date: 2017/4/26 15:54
 */
@@ -21,7 +21,19 @@ public class JavaProxyTest {
         ProxyService service = new ProxyServiceImpl();
         JavaHandle handle = new JavaHandle(service);
         Class<?> cls = service.getClass();
-        ProxyService proxy = (ProxyService) Proxy.newProxyInstance(cls.getClassLoader(),cls.getInterfaces(),handle);
-        proxy.execute("tom");
+        Object proxy = Proxy.newProxyInstance(cls.getClassLoader(),cls.getInterfaces(),handle);
+        Class clazz = proxy.getClass();
+        /**
+         * Superclass: com.sun.proxy.$Proxy0 代理类继承Proxy
+         * Superclass: java.lang.reflect.Proxy
+         * Superclass: java.lang.Object
+         */
+        while(clazz != null){
+            System.out.println("Superclass: " + clazz.getName());
+            clazz = clazz.getSuperclass();
+        }
+        System.out.println();
+        ProxyService proxyService = (ProxyService) proxy;
+        proxyService.execute("tom");
     }
 }
